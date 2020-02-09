a4cbe9a5cc57d4811b35699af4c4ee3b6d42f480
Pedro Coquito <coquito.pedro@gmail.com>
2020-02-09 20:06:57 -0300

    alteração dados

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 module.exports = {
     async index(req, res) {
         try {
-            const user  = req.user
+            const user = req.user
             return res.render("admin/users/index", { user })
         } catch (err) {
             console.error(err)
@@ -11,19 +11,22 @@ module.exports = {
     },
     async put(req, res) {
         try {
-            const id = req.session.userId
-            const { name, email } = User.findOne(req.user)
+            const { name, email, id } = req.body
 
-            await User.update(id, { name, email })
+            await User.update(id, {
+                name,
+                email
+            })
 
             return res.render('admin/users/index', {
                 user: req.user,
                 success: 'Conta atualizada com Sucesso!'
             })
+
         } catch (err) {
             console.error(err)
             return res.render('admin/users/index', {
-                error: 'Erro inesperado!'
+                error: 'Erro inesperado, tente novamente'
             })
         }
     },
